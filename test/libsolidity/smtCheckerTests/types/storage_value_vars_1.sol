d853b2cada6e5ffc465dfd2fadd2c2a734b38b7b
alliswell <151259511@qq.com>
2020-05-07 16:29:18 +0800

    update sol cases

--- test/libsolidity/smtCheckerTests/types/storage_value_vars_1.sol
+++ test/libsolidity/smtCheckerTests/types/storage_value_vars_1.sol
@@ -7,16 +7,16 @@ contract C
     function f(uint x) public {
         if (x == 0)
         {
-            a = 0x0000000000000000000000000000000000000100;
+            a = 'lat1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqgqhtfnsy';
             b = true;
         }
         else
         {
-            a = 0x0000000000000000000000000000000000000200;
+            a = 'lat1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqsqjqvzwx';
             b = false;
         }
-        assert(a > 0x0000000000000000000000000000000000000000 && b);
+        assert(a > 'lat1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq542u6a' && b);
     }
 }
 // ----
-// Warning: (362-421): Assertion violation happens here
+// Warning: (366-427): Assertion violation happens here
