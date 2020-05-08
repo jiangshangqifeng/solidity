c66a77e4495795e68b95ba4c436e6ea786f822e4
alliswell <151259511@qq.com>
2020-05-08 09:14:46 +0800

    chang address from hex to bech32

--- test/libsolidity/smtCheckerTests/types/address_balance.sol
+++ test/libsolidity/smtCheckerTests/types/address_balance.sol
@@ -3,11 +3,11 @@ pragma experimental SMTChecker;
 contract C
 {
 	function f(address a, address b) public view {
-		uint x = b.balance + 1000 ether;
+		uint x = b.balance + 1000 lat;
 		assert(a.balance > b.balance);
 	}
 }
 // ----
 // Warning: (96-102): Unused local variable.
-// Warning: (105-127): Overflow (resulting value larger than 2**256 - 1) happens here
-// Warning: (131-160): Assertion violation happens here
+// Warning: (105-125): Overflow (resulting value larger than 2**256 - 1) happens here
+// Warning: (129-158): Assertion violation happens here
--- test/libsolidity/syntaxTests/parsing/literal_constants_with_ether_subdenominations_in_expressions.sol
+++ test/libsolidity/syntaxTests/parsing/literal_constants_with_ether_subdenominations_in_expressions.sol
@@ -1,7 +1,7 @@
 contract c {
     constructor() public
     {
-         a = 1 wei * 100 wei + 7 szabo - 3;
+         a = 1 von * 100 von + 7 szabo - 3;
     }
     uint256 a;
 }
