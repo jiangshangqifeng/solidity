4cffc8639e0798ab88d28b438e17381922d1e048
alliswell <151259511@qq.com>
2020-09-25 11:29:26 +0800

    update test cases

--- test/libsolidity/syntaxTests/receiveEther/default_visibility.sol
+++ test/libsolidity/syntaxTests/receiveEther/default_visibility.sol
@@ -3,6 +3,6 @@ contract C {
     receive() {}
 }
 // ----
-// SyntaxError 4937: (95-107): No visibility specified. Did you intend to add "external"?
-// TypeError 7793: (95-107): Receive lat function must be payable, but is "nonpayable".
-// TypeError 4095: (95-107): Receive lat function must be defined as "external".
+// SyntaxError 4937: (93-105): No visibility specified. Did you intend to add "external"?
+// TypeError 7793: (93-105): Receive lat function must be payable, but is "nonpayable".
+// TypeError 4095: (93-105): Receive lat function must be defined as "external".
