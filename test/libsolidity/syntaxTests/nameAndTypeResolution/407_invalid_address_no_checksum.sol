bdb17f2c495423e136fb8f9838bc31764c21ea35
alliswell <151259511@qq.com>
2020-09-25 13:58:53 +0800

    update test cases

--- test/libsolidity/syntaxTests/nameAndTypeResolution/407_invalid_address_no_checksum.sol
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/407_invalid_address_no_checksum.sol
@@ -5,4 +5,4 @@ contract C {
     }
 }
 // ----
-// TypeError 9574: (52-108): Type literal_string "atp1lg9le9lys3vyjnxds4lp4pwuj8mlqprw2zm444" is not implicitly convertible to expected type address.
+// TypeError 9574: (52-108): Type literal_string "apt1lg9le9lys3vyjnxds4lp4pwuj8mlqprw2zm444" is not implicitly convertible to expected type address.
